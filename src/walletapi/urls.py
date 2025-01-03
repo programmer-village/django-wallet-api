4a0a45ddbdcb06c5ae9b8773c08b5b857775c51a
Никита <modaravvv@gmail.com>
2025-01-03 16:40:27 +0300

    view-atomic

--- src/walletapi/urls.py
+++ src/walletapi/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import WalletDetail, WalletListCreate  # Предположим, у вас есть вьюха для создания кошелька
+from .views import WalletDetail, perform_operation, \
+    WalletListCreate  # Предположим, у вас есть вьюха для создания кошелька
 
 urlpatterns = [
     path('api/v1/wallets/', WalletListCreate.as_view(), name='wallet_list_create'),
     path('api/v1/wallets/<uuid:uuid>/', WalletDetail.as_view(), name='wallet_detail'),
+    path('api/v1/wallets/<uuid:wallet_uuid>/operation/', perform_operation, name='perform_operation'),
 ]
--- src/walletapi/views.py
+++ src/walletapi/views.py
@@ -1,7 +1,11 @@
-from .models import Wallet
+from .models import Wallet, OperationType
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.db import transaction
+from rest_framework.exceptions import ParseError
+from django.db import IntegrityError
 from .serializers import WalletSerializer
 
 
@@ -26,3 +30,38 @@ class WalletDetail(generics.RetrieveAPIView):
             return Response({'error': 'You do not have permission to access this wallet'},
                             status=status.HTTP_403_FORBIDDEN)
         return Response(self.serializer_class(wallet).data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def perform_operation(request, wallet_uuid):
+    try:
+        with transaction.atomic():
+            wallet = Wallet.objects.select_for_update().get(uuid=wallet_uuid)
+            if wallet.user != request.user:
+                return Response({'error': 'You do not have permission to perform operations on this wallet'},
+                                status=status.HTTP_403_FORBIDDEN)
+
+            operation_type = request.data.get('operationType')
+            amount = request.data.get('amount')
+
+            if operation_type not in [OperationType.DEPOSIT, OperationType.WITHDRAW]:
+                return Response({'error': 'Invalid operation type'}, status=status.HTTP_400_BAD_REQUEST)
+            if amount <= 0:
+                return Response({'error': 'Amount must be greater than zero'}, status=status.HTTP_400_BAD_REQUEST)
+
+            if operation_type == OperationType.DEPOSIT:
+                wallet.deposit(amount)
+            elif operation_type == OperationType.WITHDRAW:
+                wallet.withdraw(amount)
+
+            return Response({'balance': str(wallet.balance)}, status=status.HTTP_200_OK)
+
+    except Wallet.DoesNotExist:
+        return Response({'error': 'Wallet not found'}, status=status.HTTP_404_NOT_FOUND)
+    except ValueError as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    except ParseError:
+        return Response({'error': 'Invalid JSON format'}, status=status.HTTP_400_BAD_REQUEST)
+    except IntegrityError:
+        return Response({'error': 'Database integrity error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
